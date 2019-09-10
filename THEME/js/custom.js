81ff1cd5a84f3139709ce599041d574c618de8c8
Gaurang Sheladiya <gaurang.sheladiya@prudlelabs.com>
2019-09-10 20:53:51 +0530

    - changes 20190910

--- THEME/js/custom.js
+++ THEME/js/custom.js
@@ -1,28 +1,78 @@
+const webUrl = 'http://www.bygoz.com/';
 const apiUrl = 'http://www.bygoz.com/public/api/';
+const pdfUrl = 'http://bygoz.com/uploads/book/pdf/';
+const frontCoverUrl = 'http://bygoz.com/public/uploads/book/front_cover/';
 
-$(document).ready(function () {
-    $('.lnkHome').click(function (e) {
+$(document).ready(function() {
+    $('.lnkHome').click(function(e) {
         e.preventDefault();
         window.location.href = "main.html";
     });
 
-    $('.lnkLibrary').click(function (e) {
+    $('.lnkLibrary').click(function(e) {
         e.preventDefault();
-        window.location.href = "download.html";
+        window.location.href = "library.html";
     });
 
-    $('.lnkStore').click(function (e) {
+    $('.lnkStore').click(function(e) {
         e.preventDefault();
         window.location.href = "store.html";
     });
 
-    $('.lnkPurchase').click(function (e) {
+    $('.lnkPurchase').click(function(e) {
         e.preventDefault();
-        window.location.href = "download.html";
+        window.location.href = "main.html";
     });
 
-    $(".lnkLogin").click(function (e) {
+    $('.lnkCart').click(function(e) {
+        e.preventDefault();
+        window.location.href = "main.html";
+    });
+
+    $('.lnkAuthors').click(function(e) {
+        e.preventDefault();
+        window.location.href = "main.html";
+    });
+
+    $('.lnkFavouriteBooks').click(function(e) {
+        e.preventDefault();
+        window.location.href = "main.html";
+    });
+
+    $('.lnkTranscationHistory').click(function(e) {
+        e.preventDefault();
+        window.location.href = "main.html";
+    });
+
+    $('.lnkMyProfile').click(function(e) {
+        e.preventDefault();
+        window.location.href = "main.html";
+    });
+
+    $('.lnkChangePassword').click(function(e) {
+        e.preventDefault();
+        window.location.href = "main.html";
+    });
+
+    $('.lnkSellWithUs').click(function(e) {
+        e.preventDefault();
+        window.location.href = "main.html";
+    });
+
+    $('.lnkLogout').click(function(e) {
+        e.preventDefault();
+        window.location.href = "main.html";
+    });
+
+    $(".lnkLogin").click(function(e) {
         e.preventDefault();
         window.location.href = "login.html";
     });
+
+    $(".lnkPurchase").css("display", "none");
+    $(".lnkCart").css("display", "none");
+    $(".lnkTranscationHistory").css("display", "none");
+    $(".lnkMyProfile").css("display", "none");
+    $(".lnkChangePassword").css("display", "none");
+    $(".lnkLogout").css("display", "none");
 });
--- viewer.js
+++ viewer.js
@@ -1,15 +1,7 @@
-
 (() => {
-
-  const viewerElement = document.getElementById('viewer');
-
-  window.WebViewer({
-    path: './public/lib',
-    initialDoc: webUrl + "uploads/book/pdf/" + window.localStorage.getItem("pdfUrl"),
-  }, viewerElement).then(instance => {
-
-    // Interact with APIs here.
-    // See https://www.pdftron.com/documentation/web/guides/basic-functionality for more info/
-  })
-
+    const viewerElement = document.getElementById('viewer');
+    window.WebViewer({
+        path: './public/lib',
+        initialDoc: pdfUrl + window.localStorage.getItem("pdfUrl"),
+    }, viewerElement).then(instance => {})
 })()
